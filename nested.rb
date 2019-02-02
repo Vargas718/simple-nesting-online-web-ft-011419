75b5256951aefe4e354aba14a16a0e7a94228755
Rafael Vargas <vargas718@gmail.com>
2019-02-02 22:43:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,16 +56,24 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
+# add the following information to the top level of programmer_hash
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+
+
+end
+ 
+ 
+
+programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
